ce894c17248196531fcfaf55554204c4cec88c3c
Kotaimen <kotaimen.c@gmail.com>
2016-01-05 16:03:23 +0800

    Add "hillshading" theme and update render script.

--- render/run_render.sh
+++ render/run_render.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-export VERSION=mk1
+export VERSION=mark1
 export RENDER_LOG=/tmp/render/render.log
 
 stonemason check
 
-stonemason tilerenderer reliefmap_${VERSION} .webp --csv=usa1-9.csv --log=${RENDER_LOG}
-stonemason tilerenderer reliefmap_${VERSION} .webp --csv=usa10-11.csv --log=${RENDER_LOG}
-stonemason tilerenderer reliefmap_${VERSION} .webp --csv=nps.csv --log=${RENDER_LOG}
-stonemason tilerenderer reliefmap_${VERSION} .webp --csv=usa3m.csv --log=${RENDER_LOG}
+stonemason tilerenderer hillshading_${VERSION} .webp --csv=usa1-9.csv --log=${RENDER_LOG}
+stonemason tilerenderer hillshading_${VERSION} .webp --csv=usa10-11.csv --log=${RENDER_LOG}
+stonemason tilerenderer hillshading_${VERSION} .webp --csv=nps.csv --log=${RENDER_LOG}
+stonemason tilerenderer hillshading_${VERSION} .webp --csv=usa3m.csv --log=${RENDER_LOG}
