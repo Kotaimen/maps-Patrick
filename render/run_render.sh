ad20ec1279527d6ea6747f9337318f32ce7afcde
Kotaimen <kotaimen.c@gmail.com>
2016-02-28 09:55:59 +0800

    Revert "Update render list."
    This reverts commit 348b41b449d8599b146560fbdc3caab7e38fb315.

--- render/run_render.sh
+++ render/run_render.sh
@@ -7,6 +7,7 @@ export RENDER_LOG=/tmp/render/render.log
 
 stonemason check
 
-stonemason tilerenderer hillshading_${VERSION} .webp --csv=terrain_4_16.csv --log=${RENDER_LOG}
-stonemason tilerenderer patrick_${VERSION} .png --csv=nps_4_15.csv --log=${RENDER_LOG}
-stonemason tilerenderer patrick_${VERSION} @2x.png --csv=nps_4_16.csv --log=${RENDER_LOG}
+stonemason tilerenderer hillshading_${VERSION} .webp --csv=usa1-9.csv --log=${RENDER_LOG}
+stonemason tilerenderer hillshading_${VERSION} .webp --csv=usa10-11.csv --log=${RENDER_LOG}
+stonemason tilerenderer hillshading_${VERSION} .webp --csv=nps.csv --log=${RENDER_LOG}
+stonemason tilerenderer hillshading_${VERSION} .webp --csv=usa3m.csv --log=${RENDER_LOG}
